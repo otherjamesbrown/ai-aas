865afc2f7b9a2749cfcfb3d92000929048c49222
James Brown <96531685+otherjamesbrown@users.noreply.github.com>
2025-11-12 13:11:25 +0000

    Fix migration: use StatementBegin/StatementEnd for dollar-quoted function

--- services/user-org-service/migrations/sql/000001_setup_function.sql
+++ services/user-org-service/migrations/sql/000001_setup_function.sql
@@ -1,12 +1,14 @@
 -- +goose Up
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+-- +goose StatementBegin
 CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS $func$
 BEGIN
   NEW.updated_at = now();
   RETURN NEW;
 END;
 $func$ LANGUAGE plpgsql;
+-- +goose StatementEnd
 
 -- +goose Down
 DROP FUNCTION IF EXISTS set_updated_at;
