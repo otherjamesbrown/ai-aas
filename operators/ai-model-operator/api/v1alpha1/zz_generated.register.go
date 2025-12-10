a99409e5e02237b9443a21c44e6cf12c4c0c9169
James Brown <otherjamesbrown@users.noreply.github.com>
2025-12-10 08:16:58 +0000

    fix(aimodel-operator): Fix unused imports and build errors

--- operators/ai-model-operator/api/v1alpha1/zz_generated.register.go
+++ operators/ai-model-operator/api/v1alpha1/zz_generated.register.go
@@ -20,21 +20,27 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	runtime "k8s.io/apimachinery/pkg/runtime"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 // GroupVersionKind returns the GVK for the AIModel.
-var (SchemeGroupVersion = schema.GroupVersion{Group: "aimodel.ai-aas.io", Version: "v1alpha1"}
-
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &runtime.SchemeBuilder{}
-
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
+var (
+	SchemeGroupVersion = schema.GroupVersion{Group: "aimodel.ai-aas.io", Version: "v1alpha1"}
+	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme        = SchemeBuilder.AddToScheme
 )
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&AIModel{},
+		&AIModelList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
 }
--- operators/ai-model-operator/main.go
+++ operators/ai-model-operator/main.go
@@ -25,7 +25,6 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
